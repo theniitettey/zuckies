d102a18609ef4944694fadadfdd88b05298563f1
michaelperryjnr <michaelperryt97@gmail.com>
2026-01-09 15:14:57 +0000

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,22 +74,7 @@ export const metadata: Metadata = {
     },
   },
   icons: {
-    icon: [
-      {
-        url: "/favicon-32.png",
-        sizes: "32x32",
-        type: "image/png",
-      },
-      {
-        url: "/favicon-16.png",
-        sizes: "16x16",
-        type: "image/png",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
+    icon: [{ url: "/favicon.ico", sizes: "any", type: "image/x-icon" }],
     apple: [
       {
         url: "/apple-touch-icon.png",
